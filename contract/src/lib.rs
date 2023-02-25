b0c9a66f847bde27a0c4d6dbdd17742dff776f1c
Hechen93 <48193329+Hechen93@users.noreply.github.com>
2023-02-24 22:48:45 -0700

    Added create_flight_details(). Modified FlightDetails and JourneyDetails structs as discussed

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,5 +1,5 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::Vector;
+use near_sdk::collections::{Vector, LookupMap};
 use near_sdk::serde::{Deserialize, Serialize};
 use near_sdk::{env, near_bindgen, AccountId};
 
@@ -11,6 +11,9 @@ pub struct JourneyDetails {
     pub passenger_status: PassengerStatus,
     pub flight_id: i64,
     pub first_name: String,
+    pub origin_city: String,
+    pub destination_city: String,
+    pub connections: Option<String>,
     pub last_name: String,
 }
 #[derive(BorshDeserialize, BorshSerialize, Debug, Serialize, Deserialize, Clone)]
@@ -35,15 +38,15 @@ pub struct InsuranceDetails {
     pub premium_amount: i32,
     pub wallet: String
 }
-#[derive(BorshDeserialize, BorshSerialize, Debug, Serialize, Deserialize, Clone)]
+#[derive(BorshDeserialize, BorshSerialize, Debug, Serialize, Deserialize)]
 pub struct FlightDetails {
     pub id: i64,
-    pub confirmation_numbers: Vector<String>,
-    pub ticket_numbers: Vector<String>,
+    pub confirmation_number: String,
+    pub ticket_number: String,
     pub airline: String,
     pub scheduled_time: u64,
-    pub estimated_departure_time: Option<u64>,
-    pub actual_departure_time: Option<u64>,
+    pub estimated_departure_time: u64,
+    pub actual_departure_time: u64,
     pub departure_city: String,
     pub arrival_city: String
 
@@ -53,147 +56,42 @@ pub struct FlightDetails {
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, Debug)]
 pub struct Contract {
-    books: Vector<Book>,
-    users: Vector<User>,
-    rents: Vector<Rent>,
+    flight_vec: Vector<FlightDetails>,
+    journey_vec: Vector<JourneyDetails>,
+    insurance_vec: Vector<InsuranceDetails>
 }
 
 impl Default for Contract {
     fn default() -> Self {
         Self {
-            books: Vector::new(b"n"),
-            users: Vector::new(b"m"),
-            rents: Vector::new(b"o"),
+            flight_vec: Vector::new(b"n"),
+            journey_vec: Vector::new(b"m"),
+            insurance_vec: Vector::new(b"o"),
         }
     }
 }
 
 #[near_bindgen]
 impl Contract {
-    pub fn new_book(&mut self, author_name: String, book_name: String, genre: Genre) {
-        let book = Book {
-            id: self.books.len() as i64,
-            author_name,
-            book_name,
-            genre,
-            rented: false,
+    pub fn new(&mut self) {
+        println!("Iterate over json objects, populate vectors");
+    }
+
+    pub fn create_flight_details(&mut self, confirmation_number: String, ticket_number: String, airline: String, 
+        scheduled_time: u64, estimated_departure_time: u64, 
+        actual_departure_time: u64, departure_city: String, arrival_city: String){
+        let flight = FlightDetails {
+            id: self.flight_vec.len() as i64,
+            confirmation_number,
+            ticket_number,
+            airline,
+            scheduled_time,
+            estimated_departure_time,
+            actual_departure_time,
+            departure_city,
+            arrival_city
         };
-        self.books.push(&book);
-    }
+        self.flight_vec.push(&flight)
 
-    pub fn get_books(&self) -> Vec<Book> {
-        let mut books = Vec::new();
-        for i in 0..self.books.len() {
-            books.push(self.books.get(i).unwrap());
-        }
-        books
-    }
-
-    pub fn get_available_books(&self) -> Vec<Book> {
-        let mut books = Vec::new();
-        for i in 0..self.books.len() {
-            let book = self.books.get(i).unwrap();
-            if book.rented == false {
-                println!("Book being pushed: {}", book.book_name);
-                books.push(book.clone());
-            }
-        }
-        books
-    }
-
-    pub fn get_rented_books(&self) -> Vec<Book> {
-        let mut books = Vec::new();
-        for i in 0..self.books.len() {
-            let book = self.books.get(i).unwrap();
-            if book.rented == true {
-                println!("Rented Book being pushed: {}", book.book_name);
-                books.push(book.clone());
-            }
-        }
-        books
-    }
-
-    pub fn remove_book(&mut self, id: i64) {
-        let index = self.books.iter().position(|book| book.id == id);
-        match index {
-            Some(i) => {
-                self.books.swap_remove(i.try_into().unwrap());
-            }
-            None => env::panic(b"Book not found"),
-        }
-    }
-
-    pub fn new_user(
-        &mut self,
-        user: String,
-        dob: i64,
-        rental_status: RentalStatus,
-        accountNumber: AccountId,
-    ) {
-        let user = User {
-            id: self.users.len() as i64,
-            user,
-            dob,
-            rental_status,
-            accountNumber,
-        };
-        self.users.push(&user);
-    }
-
-    pub fn get_users(&self) -> Vec<User> {
-        let mut users = Vec::new();
-        for i in 0..self.users.len() {
-            users.push(self.users.get(i).unwrap());
-        }
-        users
-    }
-
-    pub fn remove_user(&mut self, user_id: i64) {
-        let index = self.users.iter().position(|user| user.id == user_id);
-        match index {
-            Some(i) => {
-                self.users.swap_remove(i.try_into().unwrap());
-            }
-            None => env::panic(b"User not found"),
-        }
-    }
-
-    pub fn check_out(
-        &mut self,
-        book_id: i64,
-        user_id: i64,
-        rental_start_time: u64,
-        rental_end_time: u64,
-    ) {
-        let user = self
-            .users
-            .get((user_id - 1) as u64)
-            .expect("User not found");
-        let mut book = self
-            .books
-            .get((book_id - 1) as u64)
-            .expect("User not found");
-
-        match book.rented {
-            true => env::panic(b"Book already rented"),
-            false => {
-                book.rented = true;
-                let rent = Rent {
-                    id: self.rents.len() as i64,
-                    book_id,
-                    user_id,
-                    rental_start_time,
-                    rental_end_time,
-                };
-                self.rents.push(&rent);
-                env::log(
-                    format!(
-                        "Book '{}' has been checked out by user '{}' at {}.",
-                        book.book_name, user.user, rental_start_time
-                    )
-                    .as_bytes(),
-                );
-            }
-        }
     }
 }
